0f71d266684d129d27c28f3e997ab6b4cd2e7e08
DaniBokerone <danibokerone@gmail.com>
2024-10-02 19:18:13 +0200

    Comit amb l'activitat

---
+++ app/src/main/java/com/dani_juarez/m08_activitat1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.dani_juarez.m08_activitat1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        final Button button = findViewById(R.id.button_test);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Aixo es una proba", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
